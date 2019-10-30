9e4018f4ad183f0c614aae731f581d9e0b3a3a90
agamgn <agamgn@163.com>
2019-10-30 09:16:21 +0800

    'header'

--- vue_exam/src/router/index.js
+++ vue_exam/src/router/index.js
@@ -13,13 +13,20 @@ export default new Router({
     },
     {
       path: '/student',
-      component: () => import('@/components/student/index'),
+      component: () => import('@/components/student/Index'),
       children: [
         {path:"/",component: ()=> import('@/components/student/testpaper/Testpaper')},
         {path:"/practice",component: ()=> import('@/components/student/practice/Practice')},
         {path:"/record",component: ()=> import('@/components/student/record/Record')},
         {path:"/message",component: ()=> import('@/components/student/message/Message')},
         {path: '/manager', component: () => import('@/components/student/manager/Manager')},
+      ]
+    },
+    {
+      path: '/index', //教师主页
+      component: () => import('@/components/admin/Index'),
+      children: [
+        
       ]
     }
   ]
